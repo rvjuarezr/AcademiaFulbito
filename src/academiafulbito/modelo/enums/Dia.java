8893c94d6a7159939827050374d4e3dfb2300a16
rvjuarezr <rj20045@gmail.com>
2024-09-02 21:59:33 -0500

    avance de la ventana Horario

---
+++ src/academiafulbito/modelo/enums/Dia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package academiafulbito.modelo.enums;
+
+/**
+ *
+ * @author Ronald J
+ */
+public enum Dia {
+    Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo
+}
